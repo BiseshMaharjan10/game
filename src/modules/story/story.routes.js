62ba39af5a4af5d5abf53f50415fd6f434f109af
beebek1 <beebek990@gmail.com>
2026-07-03 03:07:16 +0545

    refactor: add player/state route to story module

--- src/modules/story/story.routes.js
+++ src/modules/story/story.routes.js
@@ -1,20 +1,31 @@
-const { Router } = require('express');
-const { authRequired } = require('../../middleware/auth');
-const { requireFields } = require('../../middleware/validate');
+const { Router } = require("express");
+const { authRequired } = require("../../middleware/auth");
+const { requireFields } = require("../../middleware/validate");
+const {
+  getPlayerStateHandler,
+} = require("../../controllers/playerState.controller");
+const {
+  setCompanyNameHandler,
+} = require("../../controllers/player.controller");
 const {
   generateStoryHandler,
   listStoryEventsHandler,
   submitChoiceHandler,
-  getPlayerStateHandler,
-  getCountryStateHandler
-} = require('../../controllers/story.controller');
+  getCountryStateHandler,
+} = require("../../controllers/story.controller");
 
 const storyRouter = Router();
 
-storyRouter.get('/stories/generate', authRequired, generateStoryHandler);
-storyRouter.get('/events/:storyId', authRequired, listStoryEventsHandler);
-storyRouter.post('/events/choice', authRequired, requireFields(['story_id', 'event_id', 'choice_index']), submitChoiceHandler);
-storyRouter.get('/player/state', authRequired, getPlayerStateHandler);
-storyRouter.get('/country/state', authRequired, getCountryStateHandler);
+storyRouter.get("/stories/generate", authRequired, generateStoryHandler);
+storyRouter.get("/events/:storyId", authRequired, listStoryEventsHandler);
+storyRouter.post(
+  "/events/choice",
+  authRequired,
+  requireFields(["story_id", "event_id", "choice_index"]),
+  submitChoiceHandler,
+);
+storyRouter.get("/player/state", authRequired, getPlayerStateHandler);
+storyRouter.post("/player/company-name", authRequired, setCompanyNameHandler);
+storyRouter.get("/country/state", authRequired, getCountryStateHandler);
 
 module.exports = { storyRouter };
